64298aec7a92ef1a21430b1a9a5422ba7b3c953d
Alkaede <andrewcast32@gmail.com>
2021-03-28 19:40:10 -0700

    completed

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,8 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
-
+* the onClick handler grabs our current state of the app and is dispatched through our reducer  
+* our reducer goes through switch cases to determine what happens to our dispatched value
+* the value is then updated in the state
+  
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useReducer }from 'react';
+// import usereducer hook 
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+// import app reducer and initialState object
+import reducer, { initialState } from './reducers/index';
+
+//import addOne, applyNumber 
+import {addOne, applyNumber, changeOperation, clearDisplay, changeMemory, addToMem, clearMem} from './actions/index';
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // create eventhandler
+  const testHandler = e =>{
+    // e.preventDefault(); dont need because nothing is being submitted for page refresh
+    dispatch(addOne(e));
+  }
+
+  const numberInput = e => {
+    dispatch(applyNumber(e));
+  }
+
+  const operation = e => {
+    dispatch(changeOperation(e));
+  }
+
+  const clear = () => {
+    dispatch(clearDisplay());
+  }
+
+  const memoryChange = () => {
+    dispatch(changeMemory());
+  }
+
+  const memoryAdd = (e) => {
+    dispatch(addToMem(e));
+  }
+
+  const memoryClear = () => {
+    dispatch(clearMem());
+  }
 
   return (
     <div className="App">
@@ -17,44 +57,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick={()=>memoryChange(state.total)} value={"M+"}/>
+              <CalcButton onClick={()=>memoryAdd(state.total)} value={"MR"}/>
+              <CalcButton onClick={()=>memoryClear()} value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={()=>numberInput(1)} value={1}/>
+              <CalcButton onClick={()=>numberInput(2)} value={2}/>
+              <CalcButton onClick={()=>numberInput(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={()=>numberInput(4)} value={4}/>
+              <CalcButton onClick={()=>numberInput(5)} value={5}/>
+              <CalcButton onClick={()=>numberInput(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={()=>numberInput(7)} value={7}/>
+              <CalcButton onClick={()=>numberInput(8)} value={8}/>
+              <CalcButton onClick={()=>numberInput(9)} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={()=>operation("+")} value={"+"}/>
+              <CalcButton onClick={()=>operation("*")} value={"*"}/>
+              <CalcButton onClick={()=>operation("-")} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={()=>clear()} value={"CE"}/>
             </div>
 
           </form>
